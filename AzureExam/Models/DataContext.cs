7bf63e1241ba998fe5bae263f6e1af22ff2c1dc0
Casko12 <phamduyhung.ftu@gmail.com>
2023-07-22 19:37:23 +0700

    exam

--- AzureExam/Models/DataContext.cs
+++ AzureExam/Models/DataContext.cs
@@ -1,6 +1,23 @@
-﻿namespace DMAWS_T2203E_PhamDuyHung.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DMAWS_T2203E_PhamDuyHung.Models
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuring the Employee entity with no primary key
+            modelBuilder.Entity<ProjectEmployee>().HasNoKey();
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<ProjectEmployee> ProjectEmployees { get; set; }
+
+
     }
 }
